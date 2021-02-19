e912ad2d252703c821fcddb481cf60bda8c6f257
mazen423 <mazen.bassiouny@cbc.de>
2021-02-19 11:47:52 +0100

    correct commit tag

--- .github/scripts/docker_build_and_push.sh
+++ .github/scripts/docker_build_and_push.sh
@@ -3,7 +3,7 @@
 export IMAGE_NAME="${REGISTRY}/${REPOSITORY}"
 echo "${SECRET}" | docker login -u "${USER}" --password-stdin https://${REGISTRY}
 docker build -f Dockerfile -t ${IMAGE_NAME}:${TREE_TAG} .
-docker tag ${IMAGE_NAME}:${TREE_TAG}:${IMAGE_NAME}:${COMMIT_TAG}
+docker tag ${IMAGE_NAME}:${TREE_TAG} ${IMAGE_NAME}:${COMMIT_TAG}
 docker push ${IMAGE_NAME}:${TREE_TAG}
 docker push ${IMAGE_NAME}:${COMMIT_TAG}
 docker logout https://${REGISTRY}
